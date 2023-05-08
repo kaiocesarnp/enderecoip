d495460851205b74199deda94304b1944b6b5de1
kaiocesarnp <kaiocesarnp@gmail.com>
2023-05-08 16:40:05 -0300

    ip

--- ip.py
+++ ip.py
@@ -14,11 +14,13 @@ print(f"Seu endereço de IP é: {ip}")
                     # HTTP: protocolo de comunicação utilizado para transferência de dados na Web.
 
 # response = requests.get('https://api.ipify.org'): Faz uma solicitação HTTP GET para a API do ipify.org usando a função get() da biblioteca requests. A resposta é armazenada na variável response.
+                    # GET: utilizado para solicitar dados de um recurso específico da API
 
 # ip = response.text: Extrai o endereço IP da resposta da API, que é retornada no formato de texto, e armazena-o na variável ip
 
-# print(f"Seu endereço de IP é: {ip}"): Imprime uma mensagem ao usuário que inclui o endereço IP retornado pela API. O uso do f-string (format string) permite que a variável ip seja incorporada diretamente na string, facilitando a leitura e a escrita do código.
-
+# print(f"Seu endereço de IP é: {ip}"): Imprime uma mensagem ao usuário que inclui o endereço IP retornado pela API. 
+                    # o f antes de uma string indica que ela é uma f-string (format string)
+                    # a f-string é uma forma de incorporar o valor de uma variável em uma string, sem precisar usar concatenação ou formatação complexa.
 
 
 
